7d6ffdd44b4ece83938e8044c5bbf50ac3f63333
robertloo <rloo@robertloo.net>
2015-02-20 00:41:04 -0500

    files added

---
+++ Downloader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deep_Paging
+{
+    class Downloader
+    {
+        // Object to store the current state, for passing to the caller. 
+        public class CurrentState
+        {
+            public int LinesCounted; // this goes to textBox1.Text  You see it as 'state.LinesCounted'
+            public string LineMessage;
+        }
+
+        public string baseAddress;
+        public string SourceFolder; // this takes form label1.Text
+        private int LinesCounted;  // you see this as LinesCounted
+
+        public void DownloadPages(System.ComponentModel.BackgroundWorker worker, System.ComponentModel.DoWorkEventArgs e)
+        {
+            CurrentState state = new CurrentState();
+            System.Net.WebClient client = new System.Net.WebClient();
+
+            string cursorMark = string.Empty;
+            string nextCursorMark = "*";
+            string path = System.IO.Path.GetFullPath(SourceFolder);
+            string dest = string.Empty;
+
+            if (System.IO.Directory.Exists(SourceFolder) == false)
+            {
+                throw new Exception("SourceFolder not specified.");
+            }
+
+            //Creates results folder if it doesn't exist already
+            string results = path + "\\results";
+            if (!System.IO.Directory.Exists(results))
+            {
+                System.IO.Directory.CreateDirectory(path + "\\results");
+            }
+          
+            while (cursorMark != nextCursorMark)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    LinesCounted += 1;
+                    dest = path + @"\results\File_" + LinesCounted + ".xml";
+
+                    while (!System.IO.File.Exists(dest))
+                    {
+                        if (worker.CancellationPending)
+                        {
+                            break;
+                        }
+                        else
+                        {                  
+                            try
+                            {
+                                cursorMark = nextCursorMark;
+                                client.DownloadFile(baseAddress + "&cursorMark=" + cursorMark, dest);
+                            }
+                            catch (System.Net.WebException exeption)
+                            {
+                                state.LineMessage = exeption.Message
+                                    + "\n" + exeption.InnerException.Message
+                                    + "\nFollowing file was not found"
+                                    + "\n" + baseAddress + "&curserMark=" + cursorMark
+                                    + "\nWill try again in 2 seconds";
+
+                                worker.ReportProgress(0, state);
+                                System.Threading.Thread.Sleep(2000); // try again in x milliseconds 
+                            }
+                        }
+                    }                    
+                    state.LinesCounted = LinesCounted;
+                    state.LineMessage = "finished processing:\n\n" + dest;
+
+                    worker.ReportProgress(0, state);
+
+                    System.Xml.Linq.XDocument xdoc = System.Xml.Linq.XDocument.Load(dest);
+                    nextCursorMark = xdoc.Root.Element("str").Value;
+                    //System.Threading.Thread.Sleep(2000);
+                }  
+            }
+            state.LineMessage = "complete!";
+            worker.ReportProgress(0, state);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Deep_Paging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            //System.Windows.Forms.OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        //Start
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartThread();
+        }
+
+        //Cancel
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Cancel the asynchronous operation. 
+            this.backgroundWorker1.CancelAsync();
+        }
+
+        private void StartThread()
+        {
+            // This method runs on the main thread. 
+
+            // Initialize the object that the background worker calls.
+            Downloader DL = new Downloader();
+            DL.baseAddress = textBox2.Text;
+            DL.SourceFolder = label1.Text;
+
+            // Start the asynchronous operation.
+            backgroundWorker1.RunWorkerAsync(DL);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // This event handler is where the actual work is done.
+            // This method runs on the background thread.
+
+            // Get the BackgroundWorker object that raised this event.
+            System.ComponentModel.BackgroundWorker worker;
+            worker = (System.ComponentModel.BackgroundWorker)sender;
+
+            // Get the Words object and call the main method.
+            Downloader DL = (Downloader)e.Argument; 
+            DL.DownloadPages(worker, e);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // This method runs on the main thread.
+            Downloader.CurrentState state = (Downloader.CurrentState)e.UserState; 
+            this.textBox1.Text = state.LinesCounted.ToString();
+            this.label2.Text = state.LineMessage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // This event handler is called when the background thread finishes. 
+            // This method runs on the main thread. 
+            if (e.Error != null)
+                MessageBox.Show("Error: " + e.Error.Message);
+            else if (e.Cancelled)
+                MessageBox.Show("File loading canceled.");
+            else
+                MessageBox.Show("Finished Loading files.");
+        }
+
+        //test button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Xml.XmlDocument xml = new System.Xml.XmlDocument();
+            xml.Load(@"C:\Users\rloo\Desktop\_temp1\test.xml");
+            System.Xml.XmlNode mynode = xml.SelectSingleNode("response/result/doc/arr[@name='subject']");
+            string myValue = mynode.InnerText;
+
+            System.Windows.Forms.MessageBox.Show(myValue);
+        }
+    }
+}
